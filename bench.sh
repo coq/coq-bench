3f2361e96324c71788920c84184a1960757bf6ed
Matej Kosik <matej.kosik@inria.fr>
2016-12-22 18:19:03 +0100

    enable benchmarking for explicitely given checkpoints on a given branch

--- bench.sh
+++ bench.sh
@@ -64,6 +64,7 @@ program_path=$(readlink -f "${program_name%/*}")
 program_name="${program_name##*/}"
 synopsys1="\t$b$program_name$r  [$b-h$r | $b--help$r]"
 synopsys2="\t$b$program_name$r  ${u}working_dir$r  ${u}coq_repository$r  ${u}coq_branch$r  ${u}num_of_iterations$r  ${u}coq_opam_package_1$r [${u}coq_opam_package_2$r  ... [${u}coq_opam_package_N$r}] ... ]]"
+synopsys3="\t$b$program_name$r  ${u}working_dir$r  ${u}coq_repository$r  ${u}coq_branch$r${b}:$r${u}head$r${b}:$r${u}base$r  ${u}num_of_iterations$r  ${u}coq_opam_package_1$r [${u}coq_opam_package_2$r  ... [${u}coq_opam_package_N$r}] ... ]]"
 
 # Print the "manual page" for this script.
 print_man_page () {
@@ -77,6 +78,8 @@ print_man_page () {
     echo -e "$synopsys1"
     echo
     echo -e "$synopsys2"
+    echo
+    echo -e "$synopsys3"
     echo
     echo -e ${b}DESCRIPTION
     echo
@@ -96,6 +99,11 @@ print_man_page () {
     echo -e "\t\t${u}num_of_iterations$r determines how many times each of the Coq OPAM packages should be compiled."
     echo
     echo -e "\t\tAll the temporary files are created inside a given ${u}working_dir$r."
+    echo
+    echo -e "$synopsys3"
+    echo
+    echo -e "\t\tLike above but instead of comparing the HEAD of the branch with the corresponding merge-base point,"
+    echo -e "\t\tcompare the commits explicitely provided by the user (${u}head$r and ${u}base$r)."
     echo
     echo -e ${b}EXAMPLES$r
     echo
@@ -201,21 +209,40 @@ fi
 # Clone the designated git-branch from the designated git-repository.
 
 coq_dir="$working_dir/coq"
-git clone -b "$coq_branch" "$coq_repository" "$coq_dir"
+git clone -b "${coq_branch%%:*}" "$coq_repository" "$coq_dir"
 cd "$coq_dir"
-echo DEBUG git commit for HEAD = $(git log | head -n 1 | awk '{print $2}')
 
-## Detect the official Coq branch
+# Detect the official Coq branch
+#
+# The computation below is based on the following assumptions:
+# - 15edfc8f92477457bcefe525ce1cea160e4c6560 is the oldest commit in "trunk" which is not present neither in "v8.6", nor in "v8.5" branches.
+# - d0afde58b3320b65fc755cca5600af3b1bc9fa82 is the oldest commit in "trunk" and "v8.6" which is not present in "v8.5".
+# - 784d82dc1a709c4c262665a4cd4eb0b1bd1487a0 is the oldest commit that is present in "trunk" and "v8.6" and "v8.5" (but not in "v8.4").
 if git log | grep 15edfc8f92477457bcefe525ce1cea160e4c6560 > /dev/null; then
     official_coq_branch=trunk
-else if git log | grep d0afde58b3320b65fc755cca5600af3b1bc9fa82 > /dev/null; then
+elif git log | grep d0afde58b3320b65fc755cca5600af3b1bc9fa82 > /dev/null; then
     official_coq_branch=v8.6
-else if git log | grep 784d82dc1a709c4c262665a4cd4eb0b1bd1487a0 > /dev/null; then
+elif git log | grep 784d82dc1a709c4c262665a4cd4eb0b1bd1487a0 > /dev/null; then
     official_coq_branch=v8.5
 else
     echo "ERROR: unrecognized Coq branch (neither \"v8.5\", nor \"v8.6\", nor \"trunk\")"
     exit 1
-fi fi fi
+fi
+
+if echo "$coq_branch" | grep '^[^:]*:[^:]*:[^:]*$' > /dev/null; then
+    head=$(echo $coq_branch | awk -F: '{print $2}')
+    base=$(echo $coq_branch | awk -F: '{print $3}')
+    coq_branch=$(echo $coq_branch | awk -F: '{print $1}')
+else
+    head=HEAD
+    git remote add upstream https://github.com/coq/coq.git
+    git fetch upstream $official_coq_branch
+    base=$(git merge-base upstream/$official_coq_branch "$coq_branch")
+fi
+
+echo DEBUG: coq_branch = $coq_branch
+echo DEBUG: head = $head
+echo DEBUG: base = $base
 
 ## Compute the OPAM version code corresponding to the compute name of the Coq branch
 case $official_coq_branch in
@@ -229,7 +256,7 @@ case $official_coq_branch in
         coq_opam_version=8.5.dev
         ;;
     *)
-        echo ERROR: unexpected value of \"coq_branch\" variable.
+        echo ERROR: unexpected value \"$official_coq_branch\" of \"official_coq_branch\" variable.
         exit 1
 esac
 
@@ -335,6 +362,9 @@ opam repo add custom-opam-repo "$custom_opam_repo"
 opam repo add coq-extra-dev https://coq.inria.fr/opam/extra-dev
 opam repo add coq-released https://coq.inria.fr/opam/released
 opam repo list
+cd "$coq_dir"
+git checkout $head
+echo DEBUG git commit for HEAD = $(git log | head -n 1 | awk '{print $2}')
 opam install coq.$coq_opam_version -v -j$number_of_processors
 
 mv "$OPAMROOT" "$OPAMROOT.HEAD"
@@ -350,12 +380,8 @@ opam repo add custom-opam-repo "$custom_opam_repo"
 opam repo add coq-extra-dev https://coq.inria.fr/opam/extra-dev
 opam repo add coq-released https://coq.inria.fr/opam/released
 opam repo list
-cd $coq_dir
-git remote add upstream https://github.com/coq/coq.git
-git fetch upstream $official_coq_branch
-base=$(git merge-base upstream/$official_coq_branch "$coq_branch")
-git checkout -b $official_coq_branch $base
 cd "$coq_dir"
+git checkout $base
 echo DEBUG git commit for BASE = $(git log | head -n 1 | awk '{print $2}')
 opam install coq.$coq_opam_version -v -j$number_of_processors
 
@@ -398,4 +424,6 @@ for coq_opam_package in $coq_opam_packages; do
     done
 done
 
+echo DEBUG 0 $num_of_iterations
+echo DEBUG 1 $(echo $num_of_iterations | wc -w)
 $program_path/bench.ml "$working_dir" $num_of_iterations $coq_opam_packages
